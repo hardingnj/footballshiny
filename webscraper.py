98a56c2b0918c3033fddee018191b2ec2a89606c
Nicholas Harding <nharding@oll-nharding.(none)>
2014-02-10 18:56:51 -0500

    updating webscraper script with looping through leagues and yaml dump

--- webscraper.py
+++ webscraper.py
@@ -6,14 +6,19 @@ import re
 import time
 import urllib2
 import codecs
+import yaml
 
 # This page is used to get the web addresses for each league table
-stattopage = urllib2.urlopen('http://www.statto.com/football/stats/england/premier-league/2011-2012').read()
+#stattopage = urllib2.urlopen('http://www.statto.com/football/stats/england/premier-league/2011-2012').read()
+stattopage = urllib2.urlopen('http://www.statto.com/football/stats/france/ligue-1/2011-2012').read()
+outfile = 'france_data.yaml'
+
 # This is the root that is appended to to get full web address
 root = 'http://www.statto.com/football/stats/'
 
 # regexes of optgroup
-patFinderTables = re.compile('<optgroup label=\"English Premier League\">(.*)</optgroup></select></div>')
+#patFinderTables = re.compile('<optgroup label=\"English Premier League\">(.*)</optgroup></select></div>')
+patFinderTables = re.compile('<optgroup label=\"French Ligue 1\">(.*)</optgroup></select></div>')
 patFinderLeagues = re.compile('option value=\"(.*?)\"')
 patFinderRows = re.compile('<td .*?<\\tr>');
 
@@ -30,24 +35,13 @@ league_data = {}
 cj = CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 for i in leagues:
-
+	
+    year = re.search( r'-(\d+)$', i).group(1)
+    print i, year
     # Paste together the URL
     url = root + i + '/table'
     print url
-    # Create an opener with the correct cookies set (OBSOLETED BY USE OF COOKIE JAR)
-    # COOOKIIIEEE JARRRR COOOKIIEEEE JARRR
-    #opener = urllib2.build_opener();
-    #opener.addheaders.append(('Cookie', 'options=DD0505030'));
-    #opener.addheaders.append(('Cookie', 'uid=3be22f9e2012-10-29CA44760b17058fd4e5c393d03ad7798146'));
-    #opener.addheaders.append(('Cookie', '__utma=144434807.1069062572.1351552161.1351552161.1351552161.1'));
-    #opener.addheaders.append(('Cookie', '__utmb=144434807.8.10.1351552161'));
-    #opener.addheaders.append(('Cookie', '__utmc=144434807'));
-    #opener.addheaders.append(('Cookie', '__utmz=144434807.1351552161.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'));
     tabpage = opener.open(url).read()
-
-    # # Print out the gotten webpage (Test code, feel free to mung until no good
-    # pageout = open('./'+ str(j) + '.html','w');
-    # pageout.write(tabpage);
 
     # Parse it with BeautifulSoup
     mysoup = BeautifulSoup(tabpage)
@@ -62,6 +56,10 @@ for i in leagues:
             line.append(text)
         dat.append(line)
     print "Champions were: ", dat[0]
-    league_data[i] = dat
+    league_data[year] = dat
     # sleep- otherwise we get in trouble with web servers.
     time.sleep( 2 )
+ 
+stream = file(outfile, 'w')
+yaml.dump(league_data, stream)
+
